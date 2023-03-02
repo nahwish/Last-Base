c501785fd4ccd9d125b3ea4403b8d0c7010dbee0
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-03-02 17:23:04 -0300

    espiral y spray funcionan contra enemigos

---
+++ Assets/ENEMIGOS/recibirDanio.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+  public class recibirDanio : MonoBehaviour {
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Finish")
+        {
+            currentHealth -= 0.2f;
+            Debug.Log("Enemy health RECIBIR PAPITOP: " + currentHealth);
+        }
+    }
+}
+
+
